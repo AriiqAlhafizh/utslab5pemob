d68e500f78a85fd7779b3c38e705d8433eae9fed
AriiqAlhafizh <ariiqalhafizh69@gmail.com>
2025-11-03 22:16:06 +0700

    add Profile Screen

---
+++ lib/Screen/profile_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:utslab5pemob/Widgets/custom_navbar.dart';
+
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/ProfileBG.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: SizedBox.shrink(),
+            ),
+            Expanded(
+              flex: 8,
+              child: Container(
+                padding:EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/KertasGede.png'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                child: Align(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/profile.png'),
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 00.0, horizontal: 20.0),
+                          decoration: BoxDecoration(
+                            // color: Colors.blue,
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(flex : 2 ,child: Text('Username')),
+                              Expanded(flex : 3 ,child: Text('AnanthaDongo')),
+                              Expanded(flex : 1 , child: Text('@')),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 00.0, horizontal: 20.0),
+                          decoration: BoxDecoration(
+                            // color: Colors.green,
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(flex : 2 ,child: Text('Highscore')),
+                              Expanded(flex : 3 ,child: Text('12345')),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: CustomNavbar(),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color(0xffe8b882),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:utslab5pemob/Screen/home_screen.dart';
+import 'package:utslab5pemob/Screen/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomeScreen(),
+      home: const ProfileScreen(),
     );
   }
 }
